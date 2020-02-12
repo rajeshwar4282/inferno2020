0dddddcc19e1f8e9344389d66f67a3376a2167da
rohith4282 <57448457+rohith4282@users.noreply.github.com>
2020-02-12 16:25:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/register', (req, res) => res.send('register'))
 app.get('/login', (req, res) => res.send('login'))
 app.get('/home',function(req,res) {
-    res.sendFile(__dirname+'/frontend/html/home.html');
+    res.sendFile(__dirname+'/frontend/index.html');
   });
   var student = {"name" : "Being Zero",
                "college" : "VNRVJIET",
